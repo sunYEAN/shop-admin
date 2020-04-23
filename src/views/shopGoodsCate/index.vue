42feb519c236c149b52137b7a6b437eb54dcf9ed
yangheng <bigyangmaster@163.com>
2020-04-23 13:52:26 +0800

    admin

--- src/views/shopGoodsCate/index.vue
+++ src/views/shopGoodsCate/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
     <el-header class="header">
-      <el-button type="primary" size="small" icon="el-icon-edit">新增</el-button>
+      <el-button type="primary" size="small" icon="el-icon-edit" @click="handleAddCate">新增</el-button>
     </el-header>
     <el-container class="main">
       <tree :data="calculateCatalogs"></tree>
     </el-container>
+
+    <el-dialog title="新增分类" :visible.sync="edit">
+      <el-form>
+        <el-form-item label="名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="简介">
+          <el-input v-model="form.front_name"></el-input>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.front_desc"></el-input>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -16,7 +30,21 @@ export default {
   data() {
     return {
       form: {
-      }
+        name: '',
+        front_name: '',
+        front_desc: '',
+        parent_id: '',
+        sort_order: 0,
+        show_index: 0,
+        is_show: 1,
+        banner_url: '',
+        icon_url: '',
+        img_ur: '',
+        wap_banner_url: '',
+        level: 'L1',
+        type: '',
+      },
+      edit: false,
     }
   },
   components: {
@@ -51,7 +79,10 @@ export default {
   methods: {
     ...mapActions('goods', [
       'getGoodsCatalogs'
-    ])
+    ]),
+    handleAddCate () {
+      this.edit = true;
+    }
   },
   mounted() {
     this.getGoodsCatalogs();
